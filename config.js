02403bd802839da1fd6c9a261126c3ded66d0495
dang78dz <100210394+dang78dz@users.noreply.github.com>
2025-05-17 22:19:30 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Thế Giới Của Emm",
+    introTitle: 'Công chúa của anhh 💖',
+    introDesc: `Giữa muôn vạn người ngoài kia,
+    Sao emmm lại chọn bước vào cuộc đời anhh?
+    Trái tim anhh từng bình yên đến lạ,
+    Nhưng từ khi gặp emmm… mọi thứ bỗng rộn ràng.`,
+    btnIntro: '^^Emmm Nhấn Đi^^',
+    title: 'Phải chăng emmm đã thương anhh từ cái nhìn đầu tiên? 😍',
+    desc: 'Hay chỉ là ánh mắt anh vô tình, nhưng tim emmm lại rung động thật lòng 🥺',
+    btnYes: 'Vì emmm yêu anhh nhiều lắm luôn đó! <33',
+    btnNo: 'Ghét nhiều lắmmm :<',
+    question: 'Trong hàng tỉ người ngoài kia, sao emmm lại chọn anhh? 💞',
+    btnReply: 'Gửi cho anhh nè 💌',
+    reply: 'Vì bên anhh, emmm thấy mình là cô gái hạnh phúc nhất 💗',
+    mess: 'Anhh biết mà 🥰. Yêu emmm nhiều thiệt nhiều 😘😘',
+    messDesc: 'Cảm ơn vì đã làm trái tim anhh biết rung động lại 🥺. Gần emm, mọi thứ như dịu dàng hơn hẳn 💞',
+    btnAccept: 'Dạaaa, emmm cũng yêu anhh <3',
+    messLink: 'https://www.facebook.com/maihaidang2602/' // Link Messenger của bạn
+}
